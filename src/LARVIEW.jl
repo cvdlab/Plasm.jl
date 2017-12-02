067d83cd72a6a4c0ba1391b5ac7526908c56dfb2
apaoluzzi <apaoluzzi@mac.com>
2017-12-02 05:57:14 +0100

    defined  and

--- src/LARVIEW.jl
+++ src/LARVIEW.jl
@@ -5,4 +5,47 @@ module LARVIEW
     
     include("./utilities.jl")
     
+    @pyimport larlib as p
+
+	function view(V::Array{Any,1},CV::Array{Any,1})
+		V = hcat(V[1],V...)
+		W = [Any[V[h,k] for h=1:size(V,1)] for k=1:size(V,2)]
+		p.VIEW(p.STRUCT(p.MKPOLS(PyObject([W,CV,[]]))))
+	end
+
+	function view(V::Array{Float64,2},CV::Array{Array{Int,1},1})
+		V = hcat(V[:,1],[V[:,k] for k in 1:size(V,2)]...)
+		W = [Any[V[h,k] for h=1:size(V,1)] for k=1:size(V,2)]
+		p.VIEW(p.STRUCT(p.MKPOLS(PyObject([W,CV,[]]))))
+	end
+
+	function viewexploded(V::Array{Any,1},CV::Array{Any,1})
+		V = hcat(V[1],V...)
+		W = [Any[V[h,k] for h=1:size(V,1)] for k=1:size(V,2)]
+		p.VIEW(p.EXPLODE(1.2,1.2,1.2)(p.MKPOLS(PyObject([W,CV,[]]))))
+	end
+
+	function viewexploded(V::Array{Any,2},CV::Array{Any,2})
+		Z = hcat(V[:,1],V)
+		W = [Any[Z[h,k] for h=1:size(Z,1)] for k=1:size(Z,2)]
+		CV = hcat(CV'...)
+		CW = [Any[CV[h,k] for h=1:size(CV,1)] for k=1:size(CV,2)]
+		p.VIEW(p.EXPLODE(1.2,1.2,1.2)(p.MKPOLS(PyObject([W,CW,[]]))))
+	end
+
+	function viewexploded(V::Array{Int64,2},CV::Array{Array{Int64,1},1})
+		Z = hcat(V[:,1],V)
+		W = [Any[Z[h,k] for h=1:size(Z,1)] for k=1:size(Z,2)]
+		CW = [Any[cell[h] for h=1:length(cell)] for cell in CV]
+		p.VIEW(p.EXPLODE(1.2,1.2,1.2)(p.MKPOLS(PyObject([W,CW,[]]))))
+	end
+
+	function viewexploded(V::Array{Float64,2},CV::Array{Array{Int64,1},1})
+		Z = hcat(V[:,1],V)
+		W = [Any[Z[h,k] for h=1:size(Z,1)] for k=1:size(Z,2)]
+		CW = [Any[cell[h] for h=1:length(cell)] for cell in CV]
+		p.VIEW(p.EXPLODE(1.2,1.2,1.2)(p.MKPOLS(PyObject([W,CW,[]]))))
+	end
+
+    
 end
---
+++ src/utilities.jl
@@ -0,0 +1,7 @@
+function array2list(cells) 
+	return PyObject([Any[cell[h] for h=1:length(cell)] for cell in cells])
+end
+
+function doublefirst(cells)
+	return p.AL([cells[1],cells])
+end
