4668e041d6cbb5775b1a8d723ae5e941d3189537
apaoluzzi <apaoluzzi@mac.com>
2017-12-28 18:14:32 +0100

    studied interface to spatial_arrangement

--- examples/double_rubik.jl
+++ examples/double_rubik.jl
@@ -53,12 +53,12 @@ end
 
 # signed operator ∂_2: C_2 -> C_1
 function boundary2(FV,EV)
-	sp_u_boundary2 = uboundary2(  FV,EV)
+	sp_u_boundary2 = uboundary2(FV,EV)
 	larEV = characteristicMatrix(EV)
 	# unsigned incidence relation
 	FE = [findn(sp_u_boundary2[f,:]) for f=1:size(sp_u_boundary2,1) ]
 	I,J,V = Int64[],Int64[],Int8[]
-	vedges = [findn(larEV[:,v]) for v=1:length(cells[1])]
+	vedges = [findn(larEV[:,v]) for v=1:size(larEV,2)]
 	for f=1:length(FE)
 		fedges = Set(FE[f])
 		col = 1
@@ -97,7 +97,7 @@ end
 
 #boundary2(FV,EV)
 
-
+#####
 
 ncubes = 3
 V,cells = LARLIB.larCuboids([ncubes,ncubes,ncubes],true)
@@ -137,4 +137,70 @@ LARVIEW.viewexploded(V',FV)
 nc = size(cscCF,1)
 CV = [collect(Set(vcat([FV[f] for f in findn(cscCF[c,:])]...)))  for c=2:nc]
 LARVIEW.viewexploded(V',CV)
+
+
+######
+
+function chaincomplex(W,FW,EW)
+	V = convert(Array{Float64,2},W')
+	EV = characteristicMatrix(EW)
+	FE = boundary2(FW,EW)
+	V,cscEV,cscFE,cscCF = LARLIB.spatial_arrangement(V,EV,FE)
+	ne,nv = size(cscEV)
+	nf = size(cscFE,1)
+	nc = size(cscCF,1)
+	EV = [findn(cscEV[e,:]) for e=1:ne]
+	FV = [collect(Set(vcat([EV[e] for e in findn(cscFE[f,:])]...)))  for f=1:nf]
+	CV = [collect(Set(vcat([FV[f] for f in findn(cscCF[c,:])]...)))  for c=2:nc]
+	function ord(cells)
+		return [sort(cell) for cell in cells]
+	end
+	temp = copy(cscEV')
+	for k=1:size(temp,2)
+		h = findn(temp[:,k])[1]
+		temp[h,k] = -1
+	end	
+	cscEV = temp'
+	return V',(ord(EV),ord(FV),ord(CV)),(cscEV,cscFE,cscCF)
+end
+
+V,(VV,EV,FV,CV) = LARLIB.larCuboids([2,2,1],true)
+V,bases,coboundaries = chaincomplex(V,FV,EV)
+EV,FV,CV = bases
+cscEV,cscFE,cscCF = coboundaries
+LARVIEW.viewexploded(V,EV)
+LARVIEW.viewexploded(V,FV)
+LARVIEW.viewexploded(V,CV)
+(ne,nv),nf,nc = size(cscEV),size(cscFE,1),size(cscCF,1)
+nv-ne+nf-nc
+
+#####
+
+function collection2model(collection)
+	W,FW,EW = collection[1]
+	shiftV = size(W,2)
+	for k=2:length(collection)
+		V,FV,EV = collection[k]
+		W = [W V]
+		FW = [FW; FV + shiftV]
+		EW = [EW; EV + shiftV]
+		shiftV = size(W,2)
+	end
+	return W,FW,EW
+end
+
+V,(VV,EV,FV,CV) = LARLIB.larCuboids([2,2,1],true)
+W,FW,EW = copy(V),copy(FV),copy(EV)
+collection = [[W,FW,EW]]
+for k=1:10
+	W,FW,EW = copy(W)+.5,copy(FV),copy(EV)
+	append!(collection, [[W,FV,EV]])
+end
+V,FV,EV = collection2model(collection)
+V,bases,coboundaries = chaincomplex(V,FV,EV)
+EV,FV,CV = bases
+LARVIEW.viewexploded(V,EV)
+LARVIEW.viewexploded(V,FV)
+LARVIEW.viewexploded(V,CV)
+
 
