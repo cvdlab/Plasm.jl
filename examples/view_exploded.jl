26f6baa2bc087181b8ed65060764f71601f8ee9e
apaoluzzi <apaoluzzi@mac.com>
2018-08-23 12:55:51 +0200

    fixed examples in view_exploded.jl

--- examples/view_exploded.jl
+++ examples/view_exploded.jl
@@ -1,3 +1,4 @@
+using LARLIB
 using LARVIEW
 using PyCall
 @pyimport pyplasm as p
@@ -15,32 +16,36 @@ model_3 = LARLIB.larModelProduct(model_2, model_1)
 
 p.VIEW(LARVIEW.lar2hpc(model_3...))
 V,CV = model_3
+
 hpc = LARVIEW.hpc_exploded( (V,[CV]) )(1.5,1.5,1.5)
 LARVIEW.view(hpc)
-LARVIEW.viewexploded(model_3[1],model_3[2])
 
-shape = (10,10,2)
+hpc = LARVIEW.lar2exploded_hpc(model_3[1], model_3[2])(1.5,1.5,1.5)
+LARVIEW.view(hpc)
+
+shape = [10,10,2]
 cubes = LARLIB.larCuboids(shape,true)
 V,FV = cubes[1],cubes[2][3]
-LARVIEW.viewexploded(V,FV)
+LARVIEW.viewexploded(V,FV)()
 
-V1,EV = LARLIB.larCuboids([30])
-W = hcat( map(u->[cos(u*(2π/30));sin(u*(2π/30))], V1 )...)
-LARVIEW.viewexploded(V1,EV)
-LARVIEW.viewexploded(W,EV)
+V,EV = LARLIB.larCuboids([30])
+V1 = hcat( map(u->[u; 0.0], V )...)
+W = hcat( map(u->[cos(u*(2π/30)); sin(u*(2π/30))], V )...)
+LARVIEW.viewexploded(V1,EV)()
+LARVIEW.viewexploded(W,EV)()
 
-V2,FV = LARLIB.larCuboids([30,6])
-V2 = [2π/30 0; 0 1/6] * V2
-W = [V2[:,k] for k=1:size(V2,2)]
+V,FV = LARLIB.larCuboids([30,6])
+V2 = [2π/30 0; 0 1/6] * V
+W = [V2[:,k] for k=1:size(V,2)]
 Z = hcat( map(p->let (u,v) = p; [v*cos(u); v*sin(u)] end, W) ...)
-LARVIEW.viewexploded(V2,FV)
-LARVIEW.viewexploded(Z,FV)
+LARVIEW.viewexploded(V2,FV)()
+LARVIEW.viewexploded(Z,FV)()
 
-V3,CV = LARLIB.larCuboids([16,32,1])
-V3 = [π/16 0 0; 0 2π/32 0; 0 0 1] * V3 
+V,CV = LARLIB.larCuboids([16,32,1])
+V3 = [π/16 0 0; 0 2π/32 0; 0 0 1] * V 
 W = [V3[:,k]-[π/2,π,1/2] for k=1:size(V3,2)] 
 Z = hcat( map(p->let (u,v,w) = p; [w*cos(u)*cos(v); w*cos(u)*sin(v); w*sin(u)] end, W) ...)
-LARVIEW.viewexploded(V3,CV)
-LARVIEW.viewexploded(Z,CV)
+LARVIEW.viewexploded(V3,CV)()
+LARVIEW.viewexploded(Z,CV)()
 
 
