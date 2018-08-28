7f9d51805b0f3199e956308838ba589f9e8448f9
apaoluzzi <apaoluzzi@mac.com>
2018-08-28 13:34:08 +0200

    removed build.jl

--- deps/build.jl
+++
@@ -1,6 +0,0 @@
-try
-   Pkg.installed("LinearAlgebraicRepresentation")
-catch
-   Pkg.clone("https://github.com/cvdlab/LinearAlgebraicRepresentation.jl.git")
-   Pkg.build("LinearAlgebraicRepresentation")
-end
