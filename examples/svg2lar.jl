a6bde869b33e97a3bc521d64718ab446ce3b1271
apaoluzzi <apaoluzzi@mac.com>
2019-02-18 09:31:20 +0100

    crumbs

--- examples/svg2lar.jl
+++ examples/svg2lar.jl
@@ -1,10 +1,13 @@
 using Plasm
 using LinearAlgebraicRepresentation
 Lar = LinearAlgebraicRepresentation
-using DataStructures
-using PyCall
-p = PyCall.pyimport("pyplasm")
 
+
+filename = "/Users/paoluzzi/Documents/dev/Plasm.jl/test/svg/path.svg"
+V, EV = Plasm.svg2lar(filename)
+Plasm.view(Plasm.numbering(.2)((V,[[[k] for k=1:size(V,2)], EV])))
+V, EV = Plasm.svg2lar(filename, normalize=false)
+Plasm.view(Plasm.numbering(.2)((V,[[[k] for k=1:size(V,2)], EV])))
 
 filename = "/Users/paoluzzi/Documents/dev/Plasm.jl/test/svg/boundarytest2.svg"
 V, EV = Plasm.svg2lar(filename)
@@ -21,3 +24,7 @@ Plasm.view(Plasm.numbering(.2)((V,[[[k] for k=1:size(V,2)], EV])))
 filename = "/Users/paoluzzi/Documents/dev/Plasm.jl/test/svg/interior.svg"
 V, EV = Plasm.svg2lar(filename)
 Plasm.view(V, EV)
+
+filename = "/Users/paoluzzi/Documents/dev/Plasm.jl/test/svg/howto.svg"
+V, EV = Plasm.svg2lar(filename)
+Plasm.view(Plasm.numbering(.2)((V,[[[k] for k=1:size(V,2)], EV])))
