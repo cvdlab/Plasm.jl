b093e7f1b1c9c974d5a43f1a4cbdccc261745861
apaoluzzi <apaoluzzi@mac.com>
2019-06-13 12:25:55 +0200

    added class MkPol da pyplasm

--- src/hpc/Convex.jl
+++ src/hpc/Convex.jl
@@ -2,6 +2,7 @@ using LinearAlgebraicRepresentation
 Lar = LinearAlgebraicRepresentation
 using QHull
 using Plasm, Revise
+GL = Plasm
 
 p = rand(10,2)
 ch = chull(p)
@@ -14,3 +15,8 @@ V = Lar.cuboid([-0.5, -0.5, -0.5])[1]
 p = convert(Array{Float64,2}, V')
 FV = chull(p).simplices
 Plasm.view(V,FV)
+
+GL.VIEW([
+      GL.GLCuboid(GL.Box3d(GL.Point3d(0,0,0),GL.Point3d(1,1,1)))
+      GL.GLAxis(GL.Point3d(-2,-2,-2),GL.Point3d(+2,+2,+2))
+      ])
